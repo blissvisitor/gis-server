a2ba8d5154829ab52108f3c64e4e99ce8b2f1361
liao <2735398076@qq.com>
2017-06-28 18:35:42 +0800

    serverline

--- routes/mobile.js
+++ routes/mobile.js
@@ -9,13 +9,32 @@ var db = require('../lib/database')
 
 router.get('/',function (req,res) {
     db.Line.findAll().then(lines =>{
-        console.log(lines[0].dataValues.geom);
-        res.send(lines[0].dataValues.geom)//,function (result) {
+        console.log(lines[4].dataValues.geom);
+        var json;
+        /*for (line in lines){
+            json += lines[l].dataValues.geom
+        }*/
+        res.send(lines)//,function (result) {
         //console.log(result)
     });
    /* res.send("get mobile")*/
 
 })
+
+router.get('/elect',function (req,res) {
+    db.elect.findAll().then(data =>{
+        console.log(data[0].dataValues.geom);
+        res.send(data);
+    })
+})
+
+router.get('/task',function (req,res) {
+    db.task.findAll().then(data =>{
+        res.send(data);
+    })
+})
+
+
 
 
 module.exports = router;
